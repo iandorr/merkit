93d9b16e07a00a64791fc2bfe03d8d9eb7a18b1b
Kuba <hortensky.jakub@gmail.com>
2022-11-08 19:50:15 +0100

    Link manager patch

--- src/plugins_app/utils.py
+++ src/plugins_app/utils.py
@@ -8,7 +8,7 @@ def get_link_manager():
     managers = LinkManager.objects.all().order_by('pk')
 
     if not managers.exists():
-        link_manager = LinkManager(number=1)
+        link_manager = LinkManager()
         link_manager.save()
 
     for i in range(1,len(managers)):
